78e80962112f722a08f1db3a3650a94eb00119f6
Yuto Arimori <n01648493@humber.ca>
2025-10-07 10:20:16 -0400

    Added cookie

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
+app.use(session(sessionConfig));
+
 
 // --- session store & config---
 const store = MongoStore.create({
