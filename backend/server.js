e6f9f4bf8d046c320fc99a7be58a7eee41a55068
Yuto Arimori <n01648493@humber.ca>
2025-10-07 10:22:00 -0400

    Edited server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('trust proxy', 1);
-app.use(session(sessionConfig));
 
 
 // --- session store & config---
