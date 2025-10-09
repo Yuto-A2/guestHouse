b59561f8b2b2ede97221aba1000a93517e41ca32
Yuto Arimori <n01648493@humber.ca>
2025-10-08 23:23:45 -0400

    trust proxy

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.set('trust proxy', 1);
+app.set('trust proxy', 1);
 
 
 // --- session store & config---
