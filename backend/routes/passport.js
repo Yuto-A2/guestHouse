11fff8a4e9923a26f9517b863f480e2f2a65f11e
Yuto Arimori <n01648493@humber.ca>
2026-04-24 22:34:20 -0400

    Update password

---
+++ backend/routes/passport.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../controllers/passport');
+const catchAsync = require('../utils/catchAsync');
+
+
+router.route('/:id')
+  .put(requireAuth, requireAdmin, catchAsync(passport.updatePassport));
